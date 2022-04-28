7ef7e5da12491587c47010c79e73349ba11a1f2c
ThunderBolt-OS <soham.bhoir@somaiya.edu>
2022-04-28 19:00:29 +0530

    initial commit

---
+++ precache-manifest.53fcd5029a931de7e0a538a85fb66df9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ef55ef2d85038cf333048399686ec8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d41d6dfe70a61fac3130",
+    "url": "/static/css/main.067038ff.chunk.css"
+  },
+  {
+    "revision": "48f7ee9373a2f0d6d4d4",
+    "url": "/static/js/2.7d7da098.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d7da098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d41d6dfe70a61fac3130",
+    "url": "/static/js/main.f44f465b.chunk.js"
+  },
+  {
+    "revision": "509df4453cf433dabd7c",
+    "url": "/static/js/runtime-main.4998cf42.js"
+  }
+]);
